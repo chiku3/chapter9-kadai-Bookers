b7cb99a8054c787a6ab4594c375e14efd442a2ca
chiku3 <p314chiku.y@gmail.com>
2021-12-19 06:42:22 +0000

    12/19-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book),notice:"Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book),notice:"Book was successfully updated."
+    else
+    @books = Book.all
+    render :edit
     end
   end
 
